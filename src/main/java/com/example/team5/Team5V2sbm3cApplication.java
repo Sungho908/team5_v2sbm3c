d595b3f1aac7eb9f45a1b69aa84dd7c986e5500b
Sungho908 <sho5693@naver.com>
2024-05-03 15:22:31 +0900

    테스트

---
+++ src/main/java/com/example/team5/Team5V2sbm3cApplication.java
@@ -0,0 +1,13 @@
+package com.example.team5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team5V2sbm3cApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team5V2sbm3cApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/team5/Team5V2sbm3cApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.team5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team5V2sbm3cApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
