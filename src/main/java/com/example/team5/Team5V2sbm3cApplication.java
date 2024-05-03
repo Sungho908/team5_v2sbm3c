cec8938a8b991836b6eb220101eea7065e14486a
Sungho908 <sho5693@naver.com>
2024-05-03 15:36:27 +0900

    두번째 전송

--- src/main/java/com/example/team5/Team5V2sbm3cApplication.java
+++ src/main/java/com/example/team5/Team5V2sbm3cApplication.java
@@ -2,8 +2,10 @@ package com.example.team5;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"dev.mvc"}) 
 public class Team5V2sbm3cApplication {
 
 	public static void main(String[] args) {
