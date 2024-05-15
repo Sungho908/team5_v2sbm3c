6c68cf4fa850e9efeb0c1dfb84c5531ad1876c1e
seungwon54 <gkstmddnjs54@gmail.com>
2024-05-16 00:09:50 +0900

    layout

---
+++ src/main/resources/static/js/dropdown.js
@@ -0,0 +1,143 @@
+// dropdown.js
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  var dropdownTrigger1 = document.getElementById("navbarDropdown1");
+  var dropdownTrigger2 = document.getElementById("navbarDropdown2");
+  var dropdownTrigger3 = document.getElementById("navbarDropdown3");
+  var dropdownTrigger4 = document.getElementById("navbarDropdown4");
+  var dropdownTrigger5 = document.getElementById("navbarDropdown5");
+  var dropdownTrigger7 = document.getElementById("navbarDropdown7");
+  var dropdownTrigger9 = document.getElementById("navbarDropdown9");
+  var dropdownMenu1 = dropdownTrigger1.nextElementSibling;
+  var dropdownMenu2 = dropdownTrigger2.nextElementSibling;
+  var dropdownMenu3 = dropdownTrigger3.nextElementSibling;
+  var dropdownMenu4 = dropdownTrigger4.nextElementSibling;
+  var dropdownMenu5 = dropdownTrigger5.nextElementSibling;
+  var dropdownMenu7 = dropdownTrigger7.nextElementSibling;
+  var dropdownMenu9 = dropdownTrigger9.nextElementSibling;
+
+
+  //---------------------------------------------------------------------
+
+  dropdownTrigger1.addEventListener("mouseenter", function() {
+    dropdownMenu1.classList.add("show");
+  });
+
+  dropdownTrigger1.addEventListener("mouseleave", function() {
+    dropdownMenu1.classList.remove("show");
+  });
+
+  dropdownMenu1.addEventListener("mouseenter", function() {
+    dropdownMenu1.classList.add("show");
+  });
+
+  dropdownMenu1.addEventListener("mouseleave", function() {
+    dropdownMenu1.classList.remove("show");
+  });
+  //---------------------------------------------------------------------
+  dropdownTrigger2.addEventListener("mouseenter", function() {
+    dropdownMenu2.classList.add("show");
+  });
+
+  dropdownTrigger2.addEventListener("mouseleave", function() {
+    dropdownMenu2.classList.remove("show");
+  });
+
+  dropdownMenu2.addEventListener("mouseenter", function() {
+    dropdownMenu2.classList.add("show");
+  });
+
+  dropdownMenu2.addEventListener("mouseleave", function() {
+    dropdownMenu2.classList.remove("show");
+  });
+  //---------------------------------------------------------------------
+  dropdownTrigger3.addEventListener("mouseenter", function() {
+    dropdownMenu3.classList.add("show");
+  });
+
+  dropdownTrigger3.addEventListener("mouseleave", function() {
+    dropdownMenu3.classList.remove("show");
+  });
+
+  dropdownMenu3.addEventListener("mouseenter", function() {
+    dropdownMenu3.classList.add("show");
+  });
+
+  dropdownMenu3.addEventListener("mouseleave", function() {
+    dropdownMenu3.classList.remove("show");
+  });
+
+  //---------------------------------------------------------------------
+  dropdownTrigger4.addEventListener("mouseenter", function() {
+    dropdownMenu4.classList.add("show");
+  });
+
+  dropdownTrigger4.addEventListener("mouseleave", function() {
+    dropdownMenu4.classList.remove("show");
+  });
+
+  dropdownMenu4.addEventListener("mouseenter", function() {
+    dropdownMenu4.classList.add("show");
+  });
+
+  dropdownMenu4.addEventListener("mouseleave", function() {
+    dropdownMenu4.classList.remove("show");
+  });
+  //---------------------------------------------------------------------
+  dropdownTrigger5.addEventListener("mouseenter", function() {
+    dropdownMenu5.classList.add("show");
+  });
+
+  dropdownTrigger5.addEventListener("mouseleave", function() {
+    dropdownMenu5.classList.remove("show");
+  });
+
+  dropdownMenu5.addEventListener("mouseenter", function() {
+    dropdownMenu5.classList.add("show");
+  });
+
+  dropdownMenu5.addEventListener("mouseleave", function() {
+    dropdownMenu5.classList.remove("show");
+  });
+  //---------------------------------------------------------------------
+
+  //---------------------------------------------------------------------
+
+  //---------------------------------------------------------------------
+  dropdownTrigger7.addEventListener("mouseenter", function() {
+    dropdownMenu7.classList.add("show");
+  });
+
+  dropdownTrigger7.addEventListener("mouseleave", function() {
+    dropdownMenu7.classList.remove("show");
+  });
+
+  dropdownMenu7.addEventListener("mouseenter", function() {
+    dropdownMenu7.classList.add("show");
+  });
+
+  dropdownMenu7.addEvetListener("mouseleave", function() {
+    dropdownMenu7.classList.remove("show");
+  });
+  //---------------------------------------------------------------------
+  dropdownTrigger9.addEventListener("mouseenter", function() {
+    dropdownMenu9.classList.add("show");
+  });
+
+  dropdownTrigger9.addEventListener("mouseleave", function() {
+    dropdownMenu9.classList.remove("show");
+  });
+
+  dropdownMenu9.addEventListener("mouseenter", function() {
+    dropdownMenu9.classList.add("show");
+  });
+
+  dropdownMenu9.addEventListener("mouseleave", function() {
+    dropdownMenu9.classList.remove("show");
+  });
+  //---------------------------------------------------------------------
+
+  //---------------------------------------------------------------------
+
+});
