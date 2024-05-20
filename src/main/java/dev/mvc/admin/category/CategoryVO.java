5fcc727af11a293bf5f3b394b7a49c7ec343ad05
Sungho908 <sho5693@naver.com>
2024-05-20 10:00:48 +0900

    xml 수정

--- src/main/java/dev/mvc/admin/category/CategoryVO.java
+++ src/main/java/dev/mvc/admin/category/CategoryVO.java
@@ -23,30 +23,30 @@ import lombok.Setter;
 @Setter @Getter
 public class CategoryVO {
   /** 카테고리 번호 */
-  private Integer c_no = 0;
+  private Integer categoryno = 0;
   
   /** 중분류명 */
   @NotEmpty(message="중분류명은 필수 입력 항목입니다.")
   @Size(min=1, max=10, message="중분류명의 입력 글자 수는 최소 1자에서 10자 이어야 합니다.")
-  private String c_name;
+  private String name;
   
   /** 소분류명 */
   @NotEmpty(message="소분류명은 필수 입력 항목입니다.")
   @Size(min=1, max=30, message="소분류명의 입력 글자 수는 최소 1자에서 30자(한글 10자) 이어야합니다.")
-  private String c_subname;
+  private String subname;
   
   /** 출력순서 */
   @NotNull(message="출력 순서는 필수 입력 항목입니다.")
   @Min(value = 1)
   @Max(value = 1000000)
-  private Integer c_seq;
+  private Integer seq;
   
   /** 항목 수 */
   @NotNull(message="항목 수는 필수 입력 항목입니다.")
   @Min(value = 1)
   @Max(value = 1000000)
-  private Integer c_cnt;
+  private Integer cnt;
   
   /** 카테고리 부모 번호 */
-  private Integer c_parent;
+  private Integer parentno;
 }
